18859a732edcf1c4bcab0f653dd8d127cb676839
totozunino <mathiaszunino@gmail.com>
2023-04-28 16:26:19 -0300

    feat: add validation pipe

--- apps/api-service/src/main.ts
+++ apps/api-service/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('API Service')
